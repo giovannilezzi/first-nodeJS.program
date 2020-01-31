f73d1880c82f1ad3d3abe8ad85b3c05b6246a5cb
Giovanni Lezzi <giovanni.lezzi@student.univaq.it>
2020-01-31 09:44:56 +0000

    .

--- src/Node-JS/makeAxiosRequest.js
+++ src/Node-JS/makeAxiosRequest.js
@@ -1,28 +1,40 @@
-const axios = require('axios')
+const axios = require("axios");
 
-axios.get('https://5e32bfe7e0161c00140abb4d.mockapi.io/api/nodeJS/users')
-  .then(function (response) {
-    // handle success
-    console.log(response);
+axios
+  .get("https://5e32bfe7e0161c00140abb4d.mockapi.io/api/nodeJS/users")
+  .then(response => console.log(response))
+  .catch(error =>console.log(error))
+  .then(() => console.log('Always executed'));
+
+axios
+  .get("https://5e32bfe7e0161c00140abb4d.mockapi.io/api/nodeJS/users/1")
+  .then(response => console.log(response))
+  .catch(error =>console.log(error))
+  .then(() => console.log('Always executed'));
+
+axios
+  .post("https://5e32bfe7e0161c00140abb4d.mockapi.io/api/nodeJS/users", {
+    createdAt: "2020-01-30T11:18:24.247Z",
+    name: "new course",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/de_ascanio/128.jpg"
   })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
+  .then(res => {
+    console.log(`statusCode: ${res.statusCode}`);
+    console.log(res);
   })
-  .then(function () {
-    // always executed
-  });
+  .catch(error => console.error(error));
 
+axios
+  .put("https://5e32bfe7e0161c00140abb4d.mockapi.io/api/nodeJS/users/1", {
+    createdAt: new Date(),
+    name: "new course"
+  })
+  .then(response => console.log(response))
+  .catch(error =>console.log(error))
+  .then(() => console.log('Always executed'));
 
-axios.post('https://5e32bfe7e0161c00140abb4d.mockapi.io/api/nodeJS/users', {
-  createdAt: "2020-01-30T11:18:24.247Z",
-  name: "new course",
-  avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/de_ascanio/128.jpg"
-})
-.then((res) => {
-  console.log(`statusCode: ${res.statusCode}`)
-  console.log(res)
-})
-.catch((error) => {
-  console.error(error)
-})
+axios
+  .delete("https://5e32bfe7e0161c00140abb4d.mockapi.io/api/nodeJS/users/4")
+  .then(({ data }) => console.log(data))
+  .catch(error =>console.log(error))
+  .then(() => console.log('Always executed'));
